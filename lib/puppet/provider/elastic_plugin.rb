864368873b0caa903a9a6419f9fd1140f20d421f
Gavin Williams <gavin.williams@elastic.co>
2020-10-28 09:34:30 +0000

    Default 'JAVA_HOME' to the Elasticsearch shipped one if not provided

--- lib/puppet/provider/elastic_plugin.rb
+++ lib/puppet/provider/elastic_plugin.rb
@@ -136,7 +136,10 @@ class Puppet::Provider::ElasticPlugin < Puppet::Provider
     }
     saved_vars = {}
 
-    unless @resource[:java_home].nil? or @resource[:java_home] == ''
+    # Use 'java_home' param if supplied, otherwise default to Elasticsearch shipped JDK
+    if @resource[:java_home].nil? or @resource[:java_home] == ''
+      env_vars['JAVA_HOME'] = "#{homedir}/jdk"
+    else
       env_vars['JAVA_HOME'] = @resource[:java_home]
     end
 
